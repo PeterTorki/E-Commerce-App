2946f1a9fa7ff1109a583aa16133fa8556642e5d
Peter Torki <pjturki@gmail.com>
2025-08-14 01:52:25 +0300

    Done creatin some updates on folder structre and hide headers of navigation and safe area wrapper component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,42 +1,14 @@
 import React from "react";
-import { View, Image, StyleSheet, ScrollView, Text } from "react-native";
-import ScreenContainer from "@/components/common/ScreenContainer";
+import { View, Text, StyleSheet } from "react-native";
 
 const Home = () => {
   return (
-    <ScrollView style={styles.container} contentContainerStyle={{ flexGrow: 1 }}>
-      <View>
-        <Image style={styles.mainImg} source={require("@/assets/home/")} />
-        <View style={styles.absoluteBox}>
-          <Text style={styles.textImage}>Fashion Sale</Text>
-        </View>
-      </View>
-    </ScrollView>
+    <View>
+      <Text>Home</Text>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  mainImg: {
-    width: "100%",
-    height: 628,
-    resizeMode: "cover",
-  },
-  absoluteBox: {
-    position: "absolute",
-    left: 0,
-    bottom: 0,
-  },
-  textImage: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#fff",
-    textAlign: "center",
-    paddingHorizontal: 20,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default Home;
